ecffe4d25fe4a38f488a79d14dbe186610271608
Sulbha <33704683+SulbhaAgg@users.noreply.github.com>
2020-11-03 19:00:57 -0500

    put test and train -> neg and positive reviews into 2 different text files

--- Homework2/NB.py
+++ Homework2/NB.py
@@ -1,11 +1,28 @@
 import os
 
 
-mainFolder = "movie-review-HW2/aclImdb/test"
-for subdirs in os.listdir(mainFolder):
-     folder = os.path.join(mainFolder, subdirs)
-     for filename in os.listdir(folder):
+def reviewsIntoFile(mainFolder , fileName ):
+    outFile = open( fileName , "w", encoding="utf8")
+    for subdirs in os.listdir(mainFolder):
+         folder = os.path.join(mainFolder, subdirs)
+         for filename in os.listdir(folder):
             temp1 = os.path.join(folder, filename)
-            files1 = open(temp1, 'r', encoding= 'utf-8')
-          ...................
-           files1.close()
+            inputFile = open(temp1, 'r', encoding= 'utf-8')
+            Lines = inputFile.readlines()
+            for line in Lines:
+                if subdirs == "neg":
+                    outFile.write( "neg " + line + "\n")
+                else:
+                    outFile.write("pos " + line + "\n")
+
+            inputFile.close()
+    outFile.close()
+
+mainFolder = "movie-review-HW2/aclImdb/test"
+reviewsIntoFile(mainFolder , "testReviews" )
+
+mainFolder = "movie-review-HW2/aclImdb/train"
+reviewsIntoFile(mainFolder , "trainReviews" )
+
+
+

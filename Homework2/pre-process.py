b3a99123e034796d2c7501bcd49312b0af856422
Sulbha <33704683+SulbhaAgg@users.noreply.github.com>
2020-11-12 22:28:59 -0500

    Pre-processing of the file using arguments

--- Homework2/pre-process.py
+++ Homework2/pre-process.py
@@ -1,8 +1,13 @@
+# Sulbha Aggarwal - NLP Homework 2
+# Prof Alla Rozovskaya
+
 import os
 import json
+import sys
+import math
 
-
-vocabulary = set([line.rstrip() for line in open('movie-review-HW2/aclImdb/imdb.vocab')])
+vocab_file = sys.argv[5]
+vocabulary = set([line.rstrip() for line in open(vocab_file)])
 
 def removeUniqueWords(words):
     return [word for word in words if word in vocabulary]
@@ -25,7 +30,7 @@ def reviewsIntoFile(mainFolder , feature_values ):
             for filename in os.listdir(folder):
                 if filename.endswith(".txt"):
                     file = os.path.join(folder, filename)
-                    inputFile = open( file, 'r', encoding= 'utf-8')
+                    inputFile = open( file, 'r' )
                     Line = inputFile.read()
                     processedText = ""
                     for character in Line:
@@ -39,26 +44,38 @@ def reviewsIntoFile(mainFolder , feature_values ):
                     feature_values.append( { subdirs: features })
                     inputFile.close()
 
+# for train
+feature_values = []
+
+# train folder for actual data is "movie-review-HW2/aclImdb/train" and train folder for the small corpus is "movie-review-small/train"
+train_folder = sys.argv[1]
+
+reviewsIntoFile(train_folder , feature_values )
+
+# train output folder for actual data is movie-review-train.NB and train output folder for the small corpus is movie-review-small-train.NB
+train_output = sys.argv[3]
+output_file = open( train_output , 'w' )
+
+for vector in feature_values:
+    output_file.writelines(json.dumps(vector) + "\n")
+
+
 # for test
 feature_values = []
 
-mainFolder = "movie-review-HW2/aclImdb/test"
-reviewsIntoFile(mainFolder , feature_values )
+# test folder for actual data is "movie-review-HW2/aclImdb/test" and test folder for the small corpus is "movie-review-small/test"
+test_folder = sys.argv[2]
 
-output_file = open("movie-review-test" + ".NB", 'w', encoding="utf8")
+reviewsIntoFile( test_folder , feature_values )
 
-for vector in feature_values:
-    output_file.writelines(json.dumps(vector) + "\n")
-
-# for train
-feature_values = []
-mainFolder = "movie-review-HW2/aclImdb/train"
-reviewsIntoFile(mainFolder , feature_values )
-
-output_file = open("movie-review-train" + ".NB", 'w', encoding="utf8")
+# test output folder for actual data is movie-review-test.NB and test output folder for the small corpus is movie-review-small-test.NB
+test_output = sys.argv[4]
+output_file1 = open( test_output , 'w' )
 
 for vector in feature_values:
-    output_file.writelines(json.dumps(vector) + "\n")
+    output_file1.writelines(json.dumps(vector) + "\n")
+
+
 
 
 
